887b9d381a13c2ef3fd5f2c50a7b2d8b43e88897
abdallah <aberlindo.mahmoud@gmail.com>
2024-10-30 08:58:56 +0300

    circular done

--- src/linkedlist/App.java
+++ src/linkedlist/App.java
@@ -2,15 +2,15 @@ package linkedlist;
 
 public class App {
     public static void main(String[] args) {
-    
-        DoublyLl list = new DoublyLl();
-        list.insertFirst(5);
-        list.insertFirst(4);
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(1);
-        list.insertLast(100);
-        list.insertAfterNode(55, 4);
+        
+        CircularLl list = new CircularLl();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
         list.display();
+
     }
 }
