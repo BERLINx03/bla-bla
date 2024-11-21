121aea295c36fd16e5a18e8c03bf9a162dabe7e7
abdallah <abdallahelsokkary4399@gmail.com>
2024-11-21 03:15:20 +0200

    cycle detection

--- src/linkedlist/Linkedlist.java
+++ src/linkedlist/Linkedlist.java
@@ -4,18 +4,68 @@ public class Linkedlist {
 
     private Node head;
     private Node tail;
-    private int size;
+    private final int size;
 
     public Linkedlist(){
         this.size = 0;
     }
-    
 
-    class Node{
+
+    public void removeDuplicateFromSorted(Node head){
+        Node currentNode = head;
+        while(currentNode != null && currentNode.next != null){
+            if(currentNode.val == currentNode.next.val){
+                currentNode.next = currentNode.next.next;
+            }else {
+                currentNode = currentNode.next;
+            }
+        }
+
+    }
+
+
+    public Node mergeTwoLists(Node list1, Node list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        Node dummy = new Node();
+        Node current = dummy;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = list1 == null ? list2 : list1;
+
+        return dummy.next;
+
+    }
+
+    public boolean hasCycle(Node head) {
+        if (head == null) return false;
+        Node tortoise = head;
+        Node hare = head;
+        while(hare.next != null && hare.next.next != null){
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if(tortoise == hare) return true;
+        }
+        return false;
+    }
+
+
+    private class Node{
 
         int val;
         Node next;
 
+        Node(){}
         Node(int val){
             this.val = val;
         }
